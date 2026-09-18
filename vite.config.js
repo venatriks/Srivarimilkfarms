15fa6e0bd2b22041e3b92ed1b2b77dbb9745c41d
Venatriks <Venatriks07@gmail.com>
2026-09-18 10:30:23 +0530

    fixing

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Srivarimilkfams/'
+  base: '/Srivarimilkfarms/'
 })
-
